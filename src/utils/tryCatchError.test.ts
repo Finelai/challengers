138677d5d940faef73abc0a7efe739298c16d39e
Finelai <5221032+Finelai@users.noreply.github.com>
2024-07-05 18:55:20 +0200

    add-typescript-eslint

--- src/utils/tryCatchError.test.ts
+++ src/utils/tryCatchError.test.ts
@@ -1,22 +1,6 @@
 import tryCatchError from './tryCatchError';
 
 describe('function tryCatchError', () => {
-  it('return Error message in string', async () => {
-    const someErrMessage = 'some error message';
-
-    function getError() {
-      try {
-        throw new Error(someErrMessage);
-      } catch (error) {
-        return someErrMessage === tryCatchError(error);
-      }
-    }
-
-    const isErrorsEqual = await getError();
-
-    expect(isErrorsEqual).toBeTruthy;
-  });
-
   it('return string if error is string', () => {
     const someErrMessage = 'some error message';
 
@@ -28,7 +12,7 @@ describe('function tryCatchError', () => {
   });
 
   it('return string if error is any object', () => {
-    const someErrObj = {go: 'some error message'};
+    const someErrObj = { go: 'some error message' };
     const someErrObjStringify = JSON.stringify(someErrObj);
 
     const errorResult = tryCatchError(someErrObj);
@@ -40,7 +24,7 @@ describe('function tryCatchError', () => {
 
   it('return string if error is object with message', () => {
     const someErrMessage = 'some error message';
-    const someErrMessageObj = {message: someErrMessage};
+    const someErrMessageObj = { message: someErrMessage };
 
     const errorResult = tryCatchError(someErrMessageObj);
     const errorResultType = typeof errorResult;
