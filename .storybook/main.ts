5541c38a93822ea718d9200ead7405f63bb2675a
Finelai <5221032+Finelai@users.noreply.github.com>
2024-07-24 18:10:24 +0200

    add storybook testing

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     '@storybook/addon-interactions',
     '@whitespace/storybook-addon-html',
     '@storybook/addon-viewport',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/nextjs',
